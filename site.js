65dd4dd1030d700e967a5a0d9f36be8daaec1d3b
Sara Victoria Lopez <sara.v.lopez49@gmail.com>
2017-11-08 14:50:04 -0600

    Fixed the form and added console logs

--- site.js
+++ site.js
@@ -1,42 +1,41 @@
-var data = [{
-  "staId": "40380",
-  "stpId": "30374",
-  "staNm": "Clark\/Lake",
-  "rt": "Blue",
-  "destNm": "UIC-Halsted",
-  "trDr": "5",
-  "arrT": "2017-11-07T16:03:09",
-  "isApp": "0",
-  "isDly": "0",
-  "heading": "123"
-}, {
-  "staId": "40380",
-  "stpId": "30375",
-  "staNm": "Clark\/Lake",
-  "rt": "Blue",
-  "destNm": "O'Hare",
-  "trDr": "1",
-  "arrT": "2017-11-07T16:11:11",
-  "isApp": "0",
-  "isDly": "0",
-  "heading": "89"
-}]
+var data = {
+  "40380": {
+    "staId": "40380",
+    "stpId": "30374",
+    "staNm": "Clark\/Lake",
+    "rt": "Blue",
+    "destNm": "UIC-Halsted",
+    "trDr": "5",
+    "arrT": "2017-11-07T16:03:09",
+    "isApp": "0",
+    "isDly": "0",
+    "heading": "123"
+  },
+  "40380": {
+    "staId": "40380",
+    "stpId": "30375",
+    "staNm": "Clark\/Lake",
+    "rt": "Blue",
+    "destNm": "O'Hare",
+    "trDr": "1",
+    "arrT": "2017-11-07T16:11:11",
+    "isApp": "0",
+    "isDly": "0",
+    "heading": "89"
+  }
+}
 
 $('#locationform').on('submit', function(e) {
+  console.log("Form submitted!");
   var location = $('#location').val();
+  console.log("Location is " + location);
   if(data[location] !== undefined ) {
-    $('#results').append('<li><a href="' + data[location]["arrT"] + '">' +
-    location + '</a></li>');
+    $('#results').append('<li><a>' + data[location]["arrT"] + '</a></li>');
+    console.log("The arrival time for the " + data[location]["rt"] + " line train heading "
+    + data[location]["trDr"] + " is " + data[location]["arrT"] );
   } else {
-    $('#result').append('<li>' + location + ' not found: it may not be in the system yet; try "Galvin library"');
+    $('#result').append('<li>' + location + ' not found');
+    console.log("Station has no arrivals");
   }
   e.preventDefault();
 });
-$('#locationform').on('focus', function() {
-    console.log('The station entered is not found. No "e"');
-  });
-
-var today = new Date();
-var date = today.getFullYear() + ' '+ (today.getMonth()+1) + ' ' + today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date + ' ' + time;
