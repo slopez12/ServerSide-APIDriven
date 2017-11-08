992028e6b3d8ec1d067c5370197a405c2121b322
Sara Victoria Lopez <sara.v.lopez49@gmail.com>
2017-11-08 14:59:22 -0600

    Added extra set of data that shows the difference

--- site.js
+++ site.js
@@ -25,6 +25,30 @@ var data = {
   }
 }
 
+// var data = [{
+//   "staId": "40380",
+//   "stpId": "30374",
+//   "staNm": "Clark\/Lake",
+//   "rt": "Blue",
+//   "destNm": "UIC-Halsted",
+//   "trDr": "5",
+//   "arrT": "2017-11-07T16:03:09",
+//   "isApp": "0",
+//   "isDly": "0",
+//   "heading": "123"
+// }, {
+//   "staId": "40380",
+//   "stpId": "30375",
+//   "staNm": "Clark\/Lake",
+//   "rt": "Blue",
+//   "destNm": "O'Hare",
+//   "trDr": "1",
+//   "arrT": "2017-11-07T16:11:11",
+//   "isApp": "0",
+//   "isDly": "0",
+//   "heading": "89"
+// }]
+
 $('#locationform').on('submit', function(e) {
   console.log("Form submitted!");
   var location = $('#location').val();
